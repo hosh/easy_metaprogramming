a77bb4d26d33c1c6c9af8d2f56f2a7f6cc6167f8
Ho-Sheng Hsiao <hosh@sparkfly.com>
2010-07-27 21:28:31 -0400

    [ARCH] rails new easy_metaprogramming

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EasyMetaprogramming::Application.config.secret_token = '4f1d919f99b2a559ce3ba45b60647f9d8a88059d26cd65b67987eb05785c4bb746f434f37eae092659b6cc3e932783491767206dd2ece64fb314151d67b35ba4'
